a3b42afd2506c27ceb3142b57b7c4fb74b97844c
Javier Quiroga <contact@javierquiroga.com>
2025-11-05 17:58:08 +0000

    feat: Set up project

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'modern-normalize/modern-normalize.css';
+
+import './main.css';
+import routes from './routes';
+
+const router = createBrowserRouter(routes, { basename: '/sudoku' });
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
